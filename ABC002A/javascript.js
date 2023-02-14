b859b6f37d77b01b8ddfbe5b67b03a7e55802b9f
csharpyth0n <121863991+csharpython@users.noreply.github.com>
2023-02-15 07:08:27 +0900

    Create javascript.js

---
+++ ABC002A/javascript.js
@@ -0,0 +1,6 @@
+// atcoder.jp/contests/abc002/submissions/38895084
+function main(input) {
+  const a = input.split(" "); 
+  console.log(Math.max(Number(a[0]),Number(a[1])));
+}
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
