18c1223758d923d0551cd6e46bf18608da6be4b6
csharpyth0n <121863991+csharpython@users.noreply.github.com>
2023-02-15 07:16:20 +0900

    Update cpp.cpp

--- ABC002D/cpp.cpp
+++ ABC002D/cpp.cpp
@@ -1,35 +1,27 @@
-// atcoder.jp/contests/abc002/submissions/32077689
+// atcoder.jp/contests/abc002/submissions/32077689 一部改変
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n,m,tomoa,tomob,max=1,count;
+    int n,m,tomoa,tomob,maxes=1,count;
     cin >> n >> m;
     vector<int> hatou(n);
     vector<vector<bool>> siriai(n, vector<bool>(n));
     for(int i=0;i<m;i++){
         cin >> tomoa >> tomob;
-        siriai.at(tomoa-1).at(tomob-1) = true;
+        siriai[tomoa-1][tomob-1] = true;
     }
     for(int bit=0;bit<(1<<n);bit++){
         count=0;
         for(int i=0;i<n;i++){
             if(bit&(1<<i)){
-                hatou.at(count)=i;
+                hatou[count]=i;
                 count++;
             }
         }
-        for(int i=0;i<count;i++){
-            for(int j=i+1;j<count;j++){
-                if(!siriai.at(hatou.at(i)).at(hatou.at(j))){
-                    goto badhatou;
-                }
-            }
-        }
-        if(count>max){
-            max=count;
-        }
+        for(int i=0;i<count;i++)for(int j=i+1;j<count;j++)if(!siriai[hatou[i]][hatou[j]])goto badhatou;
+        maxes=max(maxes,count);
         badhatou:
         n=n;
     }
-    cout << max << endl;
+    cout << maxes << endl;
 }
